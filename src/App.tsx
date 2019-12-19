94514b13535e6deb13779759b5ea7b210e82bf2e
Kevin Brendel <kBrendel@hotmail.de>
2019-12-19 05:55:27 +0100

    Experiments

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Editor from './components/Editor';
+import { Grid, Button } from '@material-ui/core';
 
 const App: React.FC = () => {
+
+	let [draw, setDraw] = useState(false);
+
 	return (
 		<div style={{ height: "100vh" }}>
-			<Editor />
-		</div>
+			<Grid container direction="row" alignItems="stretch" style={{ width: "100%", height: "100%" }}>
+				<Grid item style={{ width: "160px", padding: "8px", backgroundColor: "lightgrey" }}>
+
+				</Grid>
+				<div style={{ width: "160px", padding: "8px", backgroundColor: "grey" }}>
+					<Grid item container direction="column" alignItems="stretch">
+						<Button variant="contained" onClick={() => {
+							setDraw(!draw);
+						}}>
+							Polygon
+						</Button>
+					</Grid>
+				</div>
+				<Grid item style={{ flexGrow: 1 }}>
+					<Editor draw={draw} />
+				</Grid>
+			</Grid >
+		</div >
+
 	);
 }
 
--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useLayoutEffect } from 'react';
 import * as Three from 'three';
-import { OrthographicCamera, WebGLRenderer } from 'three';
+import { OrthographicCamera, WebGLRenderer, BoxGeometry } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { Canvas, Renderer, useThree, extend, useFrame } from 'react-three-fiber';
+//extend({ OrbitControls });
 
-const Editor: React.FC = () => {
+interface EditorProps {
+	draw: boolean;
+}
+
+const Editor: React.FC<EditorProps> = (props) => {
 	let canvasElement = {} as HTMLCanvasElement;
 
 	useEffect(() => {
@@ -13,13 +19,14 @@ const Editor: React.FC = () => {
 		camera.rotation.set(90, 0, 0);
 		camera.zoom = 20;
 		var renderer = new Three.WebGLRenderer({ canvas: canvasElement });
+		renderer.setClearColor("dimgrey");
 		var controls = new OrbitControls(camera, renderer.domElement);
 		controls.mouseButtons = { LEFT: Three.MOUSE.PAN, MIDDLE: Three.MOUSE.DOLLY, RIGHT: Three.MOUSE.PAN };
 		controls.touches = { ONE: Three.TOUCH.PAN, TWO: Three.TOUCH.DOLLY_PAN };
 		controls.enableRotate = false;
 
 		var geometry = new Three.BoxGeometry(10, 10, 10);
-		var material = new Three.MeshBasicMaterial({ color: 0x00ff00 });
+		var material = new Three.MeshBasicMaterial({ color: props.draw ? "red" : "blue" });
 		var cube = new Three.Mesh(geometry, material);
 		scene.add(cube);
 
@@ -40,6 +47,10 @@ const Editor: React.FC = () => {
 		};
 
 		render();
+
+		return (() => {
+			controls.dispose();
+		});
 	});
 
 	const resizeRendererToDisplaySize = (renderer: WebGLRenderer) => {
@@ -57,5 +68,44 @@ const Editor: React.FC = () => {
 	return (
 		<canvas ref={ref => (canvasElement = ref!)} style={{ width: "100%", height: "100%", display: "block" }} />
 	);
+
+
+	// function Stuff() {
+	// 	const { gl, size } = useThree();
+
+	// 	gl.setClearColor("dimgrey");
+	// 	gl.setSize(size.width, size.height);
+	// 	// var controls = new OrbitControls(camera, gl.domElement);
+	// 	// controls.mouseButtons = { LEFT: Three.MOUSE.PAN, MIDDLE: Three.MOUSE.DOLLY, RIGHT: Three.MOUSE.PAN };
+	// 	// controls.touches = { ONE: Three.TOUCH.PAN, TWO: Three.TOUCH.DOLLY_PAN };
+	// 	// controls.enableRotate = false;
+
+	// 	// controls.update();
+
+	// 	return (
+	// 		<mesh>
+	// 			<boxBufferGeometry attach="geometry" args={[10, 10, 10]} />
+	// 			<meshBasicMaterial attach="material" color={props.draw ? "red" : "blue"} />
+	// 		</mesh>
+	// 	);
+	// }
+
+	// const Controls = () => {
+	// 	const { camera, gl } = useThree();
+	// 	var controls = new OrbitControls(camera, gl.domElement);
+	// 	controls.mouseButtons = { LEFT: Three.MOUSE.PAN, MIDDLE: Three.MOUSE.DOLLY, RIGHT: Three.MOUSE.PAN };
+	// 	controls.touches = { ONE: Three.TOUCH.PAN, TWO: Three.TOUCH.DOLLY_PAN };
+	// 	controls.enableRotate = false;
+
+	// 	useFrame(_ => controls.update());
+	// 	return <mesh></mesh>;
+	// }
+
+	// return (
+	// 	<Canvas orthographic={true} camera={{ position: new Three.Vector3(0, 100, 0), rotation: new Three.Euler(90, 0, 0), zoom: 20 }} style={{ width: "100%", height: "100%", display: "block" }}>
+	// 		<Controls />
+	// 		<Stuff />
+	// 	</Canvas>
+	// );
 };
 export default Editor;
